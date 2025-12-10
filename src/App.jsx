11c8b7a8a096abad788a819b812fc4ee09136959
vignesh-vic <vignesha010@gmail.com>
2025-12-10 21:58:57 +0530

    added spinner and get movie api

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import { useEffect, useState } from 'react';
 import './App.css'
 import Search from './components/Search';
+import Spinner from './components/Spinner';
 const apiKey = import.meta.env.VITE_TMDB_API_KEY;
 const baseUrl = import.meta.env.VITE_API_BASE_URL;
 
@@ -25,21 +26,37 @@ const Card=({title})=>{
 const  App=()=> {
 const [hasLiked, setHasLiked] = useState(false);
 const [searchTerm, setSearchTerm] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
+const [errorMessage, setErrorMessage] = useState("");
+const [movieList, setMovieList] = useState([]);
+const [isLoading, setIsLoading] = useState(false);
+
 
 const fetchSearchMovies = async (query) => {
+  setIsLoading(true);
+  setErrorMessage("");
     try {
       const endpoint = `${baseUrl}/discover/movie?sort_by=popularity.desc`
       const response = await fetch(endpoint, APi_OPTIONS);
+      const data = await response.json();
+      if (data.response === "False") {
+        setErrorMessage(data.error || "No results found.");
+        setMovieList([]);
+        return;
+      }
+      setMovieList(data?.results || []);
     } catch (error) {
         console.error("Error fetching search results:", error);
       setErrorMessage("Failed to fetch search results. Please try again.")
-    } 
+    } finally {
+      setIsLoading(false);
+    }
   
   }
   useEffect(() => {
     fetchSearchMovies()
   }, []);
+
+
   return (
     <main>
 
@@ -50,9 +67,29 @@ const fetchSearchMovies = async (query) => {
         <h2 className='title'> <span className='text-purple-400'>Movies</span> You'll Enjoy Without the Hassle</h2>
         <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
        </header>
-       <section>
-        <h2>All Movies</h2>
-        {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+       <section className='flex justify-center'>
+       <div>
+            <h1 className=' text-white mt-5 mb-5 text-xl '>All Movies</h1>
+            {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+            {
+              isLoading ? (
+                <Spinner />
+              ) : errorMessage ? (<p className="text-red-500">{errorMessage}</p>) : (
+                <div className='flex flex-col gap-4'>
+                  {
+                    movieList.length > 0
+                    &&
+                    movieList.map((movie) => (
+                      <p className='font-normal text-white'>{movie.title}</p>
+
+                    )
+                    )
+
+                  }
+                </div>
+              )
+            }
+       </div>
        </section>
         </div>
     
