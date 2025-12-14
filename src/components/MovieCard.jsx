c59312b2f74f5a3e2270e6bdd0d167f1242edeec
vignesh-vic <vignesha010@gmail.com>
2025-12-14 13:06:26 +0530

    added custom movie card component with render movie mages

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 
-const MovieCard = ({movie}) => {
+const MovieCard = ({movie:{title, vote_average, poster_path, release_date, original_language }}) => {
   return (
-      <p key={movie.id} className="text-white text-xs">
-          {movie.title}
-      </p>
+    <div className='border  p-5 shadow-2xl rounded-2xl bg-gray-900 shadow-light-100/10'>
+      <img src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` :'/no-movie.png' } alt={title} />
+      <div className='mt-4'>
+        <h3 className='text-white'>{title}</h3>
+
+        <div >
+          
+          <div>
+          <img src="star.svg" alt="Start Icon" />
+          </div>
+        
+        </div>
+
+      </div>
+    </div>
   )
 }
 
